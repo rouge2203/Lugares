91371b8e07b1a888aee7e278272e3e964af12957
Alejandro <jruiz60480@ufide.ac.cr>
2022-11-30 17:16:09 -0600

    Commit clase de listado de lugares creo que semana 10

---
+++ app/src/main/java/com/example/lugares/adapter/LugarAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.lugares.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.ViewParent
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lugares.databinding.LugarFilaBinding
+import com.example.lugares.model.Lugar
+
+class LugarAdapter: RecyclerView.Adapter<LugarAdapter.LugarViewHolder>() {
+
+    private var listarLugares = emptyList<Lugar>()
+
+    inner class LugarViewHolder(private var itemBinding: LugarFilaBinding) :
+        RecyclerView.ViewHolder(itemBinding.root){
+
+    fun dibujar(lugar: Lugar) {
+        itemBinding.tvNombre.text = lugar.nombre
+        itemBinding.tvTelefono.text = lugar.telefono
+        itemBinding.tvCorreo.text = lugar.correo
+
+    }
+}
+
+    fun setLugares(lugares: List<Lugar>){
+        listarLugares = lugares
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LugarViewHolder{
+        val itemBinding = LugarFilaBinding
+            .inflate(LayoutInflater.from(parent.context)
+            ,parent,false)
+        return LugarViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: LugarViewHolder, position: Int) {
+        val lugar = listarLugares[position]
+        holder.dibujar(lugar)
+    }
+
+    override fun getItemCount(): Int{
+        return listarLugares.size
+    }
+
+}
--- app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lugares.R
+import com.example.lugares.adapter.LugarAdapter
 import com.example.lugares.databinding.FragmentLugarBinding
 import com.example.lugares.viewmodel.LugarViewModel
 
@@ -32,6 +34,20 @@ class LugarFragment : Fragment() {
 
         binding.fbAgregar.setOnClickListener{
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)  }
+
+        //Cargar datos
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter  = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel.getAllData.observe(viewLifecycleOwner){
+            lugares -> lugarAdapter.setLugares(lugares)
+        }
+
+
+
+
         return binding.root
     }
 
